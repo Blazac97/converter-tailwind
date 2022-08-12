a6769e579e7946466df0a5d2e0fcf1c6727b3be9
unknown <kartausigor97@gmail.com>
2022-08-12 10:37:22 +0300

    Add 2 btns and imputnumber area

--- src/pages/lengthpage.tsx
+++ src/pages/lengthpage.tsx
@@ -4,13 +4,25 @@ export const Lengthpage: React.FC = () => {
   const navigate = useNavigate();
   return (
     <>
-      <div className=" flex mx-auto mt-4 ">
+      <div className=" flex mx-auto mt-4 justify-center">
         <div className="flex">
           <button
-            className=" w-24 flex justify-center rounded-lg p-1  text-white bg-indigo-600/90 hover:animate-pulse"
+            className=" w-24 flex justify-center items-center rounded-l-lg  text-white h-9 bg-indigo-600/90 hover:animate-pulse"
             onClick={() => navigate("/")}
           >
             {"<RETURN"}
+          </button>
+        </div>
+        <div className="justify-center w-2/4 mx-6">
+          <input
+            type="number"
+            placeholder="Input your number"
+            className="form-input  px-4 py-3 rounded-lg h-9 text-black text-center hover:border-2 w-full"
+          ></input>
+        </div>
+        <div className="flex">
+          <button className=" w-24 flex justify-center items-center rounded-r-lg p-1  text-white h-9 bg-green-500 hover:animate-pulse">
+            {"Calculate"}
           </button>
         </div>
       </div>
